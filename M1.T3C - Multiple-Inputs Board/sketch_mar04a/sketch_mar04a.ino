1b01195f557283e0a561f651b0bba4f076c979fd
Greg McIntyre <gregmcintyre1984@gmail.com>
2019-03-04 10:29:06 +1000

    M1T3 Initial

---
+++ M1.T3C - Multiple-Inputs Board/sketch_mar04a/sketch_mar04a.ino
@@ -0,0 +1,44 @@
+/*  Interrupt-driven Board
+ *  4/3/19
+ *  Greg McIntyre
+ *  
+ *  Interrupt implementation of T1
+ *  
+ *  https://github.com/gregorymcintyre/ProgrammingParadigms.git
+ *  
+ */
+ 
+#define PIR 2
+#define SWITCH 3
+
+int input = 0;
+volatile bool LEDstate = false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Program Running...");
+  pinMode(LED_BUILTIN, OUTPUT);     //built in LED
+  pinMode(PIR, INPUT);
+  pinMode(SWITCH, INPUT);
+
+  //attachInterrupt(digitalPinToInterrupt(PIR), change, CHANGE);
+}
+
+void loop() {
+  
+  if(digitalRead(SWITCH)==0){
+    Serial.println("DOWN");
+  }
+  delay(1000);
+
+  
+  
+}
+
+void change()
+{
+  LEDstate = !LEDstate;
+  digitalWrite(LED_BUILTIN, LEDstate);
+  Serial.println("Interrupt has occured");
+}
+
