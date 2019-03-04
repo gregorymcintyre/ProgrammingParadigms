50b108c7fda26f7ee107443b0ee334fae32bb7cf
Greg McIntyre <gregmcintyre1984@gmail.com>
2019-03-04 10:08:46 +1000

    M1T2

--- M1.T2P - Interrupt-Driven Board/sketch_mar04a/sketch_mar04a.ino
+++ M1.T2P - Interrupt-Driven Board/sketch_mar04a/sketch_mar04a.ino
@@ -1,26 +1,36 @@
+/*  Interrupt-driven Board
+ *  4/3/19
+ *  Greg McIntyre
+ *  
+ *  Interrupt implementation of T1
+ *  
+ *  https://github.com/gregorymcintyre/ProgrammingParadigms.git
+ *  
+ */
+ 
 #define PIR 2
 
 int input = 0;
+volatile bool LEDstate = false;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Program Running...");
   pinMode(LED_BUILTIN, OUTPUT);     //built in LED
   pinMode(PIR, INPUT);
-  
+
+  attachInterrupt(digitalPinToInterrupt(PIR), change, CHANGE);
 }
 
 void loop() {
-  input = digitalRead(PIR);
-  //Serial.println(input);
-  if(input){
-    digitalWrite(LED_BUILTIN, 1);
-    Serial.println("Motion Detected");
-  }else{
-    digitalWrite(LED_BUILTIN, 0);
-    Serial.println("Motion Has ceased");
-  }
-  
   delay(1000);
   
 }
+
+void change()
+{
+  LEDstate = !LEDstate;
+  digitalWrite(LED_BUILTIN, LEDstate);
+  Serial.println("Interrupt has occured");
+}
+
